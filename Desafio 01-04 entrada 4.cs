359cf845fd5842e64d3e9f5c8ab7ff8cab1b3191
sofia082001 <68034198+sofia082001@users.noreply.github.com>
2020-07-14 13:46:36 -0500

    Add files via upload

---
+++ Desafio 01-04 entrada 4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingresamos los datos conocidos
+            Console.WriteLine("Ingrese el valor de w:");
+            float w = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor de t:");
+            float t = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor de c en grados:");
+            float c = float.Parse(Console.ReadLine());
+
+            //Se pasa de grados a radianes y luego se opera
+            double cRad = c * (Math.PI / 180);
+            double y = Math.Cos(cRad) * t;
+
+            double cat = (t * t) - (y * y);
+            double z = Math.Sqrt(cat);
+
+            double cat2 = (w * w) - (z * z);
+            double catetogrande = Math.Sqrt(cat2);
+            double x = catetogrande - y; 
+            Console.WriteLine("El valor de x es:" + Math.Round(x));
+        }
+    }
+}
