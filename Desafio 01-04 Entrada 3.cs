186af47903ec68c8727b6c09c758522a6b208f22
sofia082001 <68034198+sofia082001@users.noreply.github.com>
2020-07-14 13:45:25 -0500

    Add files via upload

---
+++ Desafio 01-04 Entrada 3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingresamos los datos conocidos
+            Console.WriteLine("Ingrese el valor de w:");
+            float w = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor de d en grados:");
+            float d = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor de x:");
+            float x = float.Parse(Console.ReadLine());
+
+            //Primero hallo el cateto c del triangulo mas grande
+
+            double dRad = d * (Math.PI / 180);
+            double c = Math.Cos(dRad) * w;
+
+            double y = c - x;
+            Console.WriteLine("El valor de y es:" + Math.Round(y));
+
+          
+        }
+    }
+}
